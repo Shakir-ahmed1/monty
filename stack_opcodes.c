54e2e56c510b2b0289a644460b1fede67a9204be
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-22 21:41:50 +0000

    changing stack structure

--- stack_opcodes.c
+++ stack_opcodes.c
@@ -23,7 +23,13 @@ void push(stack_t **stack, unsigned int line_number)
 	}
 	new->n = get_number(line_number);
 	new->next = NULL;
-	if (1)
+	if (*stack === NULL)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+		*stack = new;
+	}
+	else
 	{
 		/*new->prev = NULL;
 		new->next = *stack;
